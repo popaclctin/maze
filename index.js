80633cc4e1e47ff5ed408c6f2e3baba870222d01
Claudiu Popa <popa.clctin@gmail.com>
2020-09-04 17:23:37 +0300

    Generate a maze

--- index.js
+++ index.js
@@ -39,3 +39,63 @@ const verticals = Array(cells)
 const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+    const temp = arr[index];
+    arr[index] = arr[counter];
+    arr[counter] = temp;
+  }
+  return arr;
+};
+
+const startingRow = Math.floor(Math.random() * cells);
+const startingColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  if (grid[row][column]) {
+    return;
+  }
+
+  grid[row][column] = true;
+
+  const neighbours = shuffle([
+    [row - 1, column, "up"],
+    [row, column + 1, "right"],
+    [row - 1, column, "down"],
+    [row, column - 1, "left"],
+  ]);
+
+  for (let neighbour of neighbours) {
+    const [nextRow, nextColumn, direction] = neighbour;
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue;
+    }
+
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
+
+    if (direction === "left") {
+      verticals[row][column - 1] = true;
+    } else if (direction === "right") {
+      verticals[row][column] = true;
+    } else if (direction === "up") {
+      horizontals[row - 1][column] = true;
+    } else if (direction === "down") {
+      horizontals[row][column] = true;
+    }
+
+    stepThroughCell(nextRow, nextColumn);
+  }
+};
+
+stepThroughCell(startingRow, startingColumn);
